76fb37e85c602f12bea1ca6dc20836012aaa52f8
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-04-04 22:39:30 +0300

    task 5 change

--- js/task-02.js
+++ js/task-02.js
@@ -7,28 +7,6 @@ const ingredients = [
   'Приправы',
 ];
 
-// const ingredientsEl = ingredients.map(ingredient => {
-// const items = document.createElement('li');
-//     items.textContent = ingredient;
- 
-//     console.log(items);
-
-//     return items;
-
-// })
-// const ingredientsEllist = document.querySelector('#ingredients');
-// ingredientsEllist.append(...ingredientsEl);
-
-
-// let ulList = document.getElementById("ingredients");
-// const foodIngredients = ingredients.forEach(ingredient => {
-//   let items = document.createElement("li");
-//   items.innerHTML = ingredient;
-//   ulList.append(items);
-  
-// });
-
-// console.log(foodIngredients);
 
 const ulList = document.getElementById("ingredients");
 console.log(ulList);
--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,18 @@
 const input = document.querySelector('#name-input');
 const text = document.querySelector('#name-output');
 
-function onInputChange(event) {
+// function onInputChange(event) {
+//     text.textContent = event.currentTarget.value;
+//     if (!text.textContent) {
+//         text.textContent = 'незнакомец';
+//     }
+    
+// }
+// input.addEventListener('input', onInputChange);
+
+input.addEventListener('input', event => {
     text.textContent = event.currentTarget.value;
     if (!text.textContent) {
         text.textContent = 'незнакомец';
     }
-    
-}
-input.addEventListener('input', onInputChange);
+});
